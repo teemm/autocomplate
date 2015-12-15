d6f75959c69d69f8e7e34bb1c7afeede4130ee33
teemm <kverna133@gmail.com>
2015-12-15 10:09:43 +0400

    test complate

---
+++ application/controllers/test.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class test extends CI_Controller {
+	public function index(){	
+		$this->load->view('test');
+	}
+	public function coment(){
+		$arg = $this->input->get('arg');
+		$this->load->model('model');
+		echo (json_encode($this->model->keyup($arg)));
+	}
+}
---
+++ application/views/test.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script>
+
+	$(function(){
+		$('#sw_search').on('keyup', function(){
+			_html = "";
+			$.get('<?php echo base_url("test/coment"); ?>',{ coment : $(this).val() }, function(resp){
+				for (var i = 0; i < resp.length; i++) {
+					_html += '<li>' + resp[i].content + '</li>'
+				}
+				$('.uels').html(_html);
+				console.log(_html);
+			},'json');
+		});
+	});
+</script>
+<body>
+<input type="text" id="sw_search" placeholder="search" name="keyword">
+<ul class="uels">
+	
+</ul>
+
+</body>
+</html>
